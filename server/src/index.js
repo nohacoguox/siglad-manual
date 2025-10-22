9645c08c594ce6be002a7626473126954c86c375
NOE COGUOX <estuardocoguox@hotmail.com>
2025-10-22 14:41:10 -0600

    fix: listen on 0.0.0.0 for render

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
 import app from './server.js';
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`SIGLAD API listening on http://localhost:${PORT}`));
+//app.listen(PORT, () => console.log(`SIGLAD API listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`SIGLAD API listening on http://0.0.0.0:${PORT}`));
